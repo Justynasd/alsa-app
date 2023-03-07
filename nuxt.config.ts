4a8083bfdab2f6ebaa7660cf06143594069f307b
Justynasd <justynasd@gmail.com>
2023-03-07 19:37:50 +0200

    config sass i18n image-edge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,21 @@ export default defineNuxtConfig({
   typescript: {
     shim: false,
   },
-  modules: ["@nuxtjs/i18n"],
+  css: ["@/assets/scss/main.scss"],
+  image: {
+    dir: "public/img",
+    // The screen sizes predefined by `@nuxt/image`:
+    // screens: {
+    //   xs: 320,
+    //   sm: 640,
+    //   md: 768,
+    //   lg: 1024,
+    //   xl: 1280,
+    //   xxl: 1536,
+    //   '2xl': 1536,
+    // },
+  },
+  modules: ["@nuxt/image-edge", "@nuxtjs/i18n"],
   i18n: {
     locales: [
       {
